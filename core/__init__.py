e85f98f949d746bf9dc1273cbf006d3398822e4d
MileXiang <smarthomemiles@gmail.com>
2026-04-06 15:47:22 +0800

    Add files via upload

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
-# S2-SWM Indoor Governance Core Package
 from .visa_manager import SpatioTemporalVisaManager
 from .spatial_ledger import SpatialLedger
 from .lord_brain import LordGovernanceBrain
+from .robot_navigation_pipeline import S2RobotNavigationPipeline
 
-__all__ = ["SpatioTemporalVisaManager", "SpatialLedger", "LordGovernanceBrain"]
+__all__ = ["SpatioTemporalVisaManager", "SpatialLedger", "LordGovernanceBrain", "S2RobotNavigationPipeline"]
